7e12c4e2bcf01d972c90b2fbc95a867e9199f39b
Jim Geng <jimgeng2004@gmail.com>
2021-04-21 19:39:27 -0600

    sync

---
+++ src/Assignments/Lesson43ArrayList.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+import java.util.*;
+
+public class Lesson43ArrayList {
+	public static void Question1() {
+		List<String> alst = new ArrayList<String>();
+	}
+	
+	public static void Question2() {
+		// ArrayLists are restricted in that only objects can be stored in them.
+	}
+
+	public static void Question3() {
+		// The main advantage in using an ArrayList instead of a normal array is that the ArrayList is dynamic (resizable).
+	}
+
+	public static void Question4() {
+		// By creating an ArrayList with a type parameter, this ensures that
+	}
+}
